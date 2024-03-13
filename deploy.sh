4f13a861e53ca066a596ac8bb48bfd226580d93f
RubyK01 <tomaskehoe4@gmail.com>
2024-03-13 20:35:20 +0000

    yml changes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt install nodejs npm
 #Install npm install -g pm2
+sudo npm install -g pm2
 #stip any instance of our application running currently
 pm2 stop SimpleApplication
 # change directory into folder app is downloaded
